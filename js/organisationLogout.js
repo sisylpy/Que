49bf103a39dda21081ff9edc3437fcd7e3a926b3
sisylpy <454926763@qq.com>
2018-02-25 00:42:54 +0800

    Event适配完成！

--- js/organisationLogout.js
+++ js/organisationLogout.js
@@ -5,15 +5,15 @@
     window.onscroll = function () {
         if ($(window).scrollTop() > 600) {
             //事件绑定
-            $(".tool-toTop").animate({"right":"80px"},1000);
+            $(".tool-toTop").animate({"right":"80px"},500);
         }else  {
-            $(".tool-toTop").animate({"right":"0"},1000);
+            // $(".tool-toTop").animate({"right":"0"},1000);
         }
     };
     //回到顶部按钮
     $(".tool-toTop").click(function () {
-        $("html,body").animate({scrollTop:0},1000);
-        $(".tool-toTop").animate({"right":"0"},1000);
+        $("html,body").animate({scrollTop:0},200);
+        // $(".tool-toTop").animate({"right":"0"},1000);
     })
 
 
@@ -24,10 +24,21 @@
 
 
     //pc页面，点击"关闭"事件
-    $('.fa-times').click(function () {
+    $('.pc-close').click(function () {
         $('.esjy-contact-widget').animate({"right":"-380px"},500);
     })
 
+
+    //mob页面，点击"联系我们"事件
+    $('.esjy-contact-btn-mob').click(function () {
+        $('.esjy-contact-widget').animate({"bottom":"0"},500);
+    })
+
+
+    //mob页面，点击"关闭"事件
+    $('.mob-close').click(function () {
+        $('.esjy-contact-widget').animate({"bottom":"-380px"},500);
+    })
 
 
 
