54fb6eb9a4b3057a8c7b5b5d2c8a1f2952cf6bd9
sisylpy <454926763@qq.com>
2018-02-27 17:57:01 +0800

    articles begine

--- js/article-page.js
+++ js/article-page.js
@@ -4,12 +4,13 @@
 
     var navHeight = $('.Header').height();
     var bannerHeight = $('.article-page-banner').height();
-    var topHeight = navHeight + bannerHeight - (bannerHeight * 0.2) ;
+    // var topHeight = navHeight + bannerHeight - (bannerHeight * 0.2) ;
+    var topHeight = navHeight + bannerHeight - 55;
 
     window.onscroll = function () {
         if ($(window).scrollTop() > topHeight) {
             $('.mune-col').addClass('fixed');
-            $('.article-detail').css("marginTop",86.5);
+            $('.article-detail').css("marginTop",55);
         }else  {
             $('.mune-col').removeClass('fixed');
             $('.article-detail').css("marginTop",0);
